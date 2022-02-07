30c30d1e109c35d0bb1187aa768ec0bef31ae8e1
Gustavo <lgusta333@gmail.com>
2022-02-07 17:02:34 -0300

    Payment one-to-one association

--- src/main/java/com/educandoewb/course/config/TestConfig.java
+++ src/main/java/com/educandoewb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoewb.course.entities.Category;
 import com.educandoewb.course.entities.Order;
 import com.educandoewb.course.entities.OrderItem;
+import com.educandoewb.course.entities.Payment;
 import com.educandoewb.course.entities.Product;
 import com.educandoewb.course.entities.User;
 import com.educandoewb.course.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2022-1-27T21:49:01Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
